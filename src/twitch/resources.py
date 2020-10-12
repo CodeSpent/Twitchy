59e8381df376b1e17265ea25a9d823f1c718a3b4
Patrick Hanford <hanfordpatrick@gmail.com>
2020-10-12 07:02:31 -0400

    feat: adds TwitchAPIMixin for object orientation and better reusability

---
+++ src/twitch/resources.py
@@ -0,0 +1,69 @@
+from datetime import datetime
+
+
+def convert_to_twitch_object(name, data):
+    types = {"user": User}
+
+    special_types = {
+        "created_at": _DateTime,
+        "updated_at": _DateTime,
+        "started_at": _DateTime,
+        "followed_at": _DateTime,
+    }
+
+    if isinstance(data, list):
+        return [convert_to_twitch_object(name, x) for x in data]
+
+    if isinstance(data, dict) and name in types:
+        obj = types.get(name)
+        return obj.construct(data)
+
+    if name in special_types:
+        obj = special_types.get(name)
+        return obj.construct(data)
+
+    return data
+
+
+class TwitchObject(dict):
+    def __setattr__(self, name, value):
+        if name[0] == "_" or name in self.__dict__:
+            return super(TwitchObject, self).__setattr__(name, value)
+
+    def __getattr__(self, name):
+        return self[name]
+
+    def __delattr__(self, name):
+        if name[0] == "_":
+            return super(TwitchObject, self).__delattr__(name)
+
+        del self[name]
+
+    def __setitem__(self, key, value):
+        key = key.lstrip("_")
+        super(TwitchObject, self).__setitem__(key, value)
+
+    @classmethod
+    def construct(cls, values):
+        instance = cls()
+        instance.refresh(values)
+        return instance
+
+    def refresh(self, values):
+        for key, value in values.copy().items():
+            self.__setitem__(key, convert_to_twitch_object(key, value))
+
+
+class _DateTime(object):
+    @classmethod
+    def construct(cls, value):
+        if value is None:
+            return None
+        try:
+            return datetime.strptime(value, "%Y-%m-%dT%H:%M:%SZ")
+        except ValueError:
+            return datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+
+class User(TwitchObject):
+    pass
