127abffbd9ea45ed58cf30ba1a412ef8d6ee55b0
Patrick Hanford <hanfordpatrick@gmail.com>
2020-10-16 22:05:07 -0400

    fix: changes name of package to 'pytwitch' expressly
    changes name of package from 'twitch' implicit to 'pytwitch' expressly to avoid possible name conflicts while users port from previous clients to pytwitch

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-    Setup file for twitch_helix.
-    Use setup.cfg to configure your project.
-
-    This file was generated with PyScaffold 3.2.3.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
-"""
 import sys
 
 from pkg_resources import VersionConflict, require
